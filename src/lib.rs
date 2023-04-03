9359a73633d387697c141a70253ab8fa6fba1f98
root <jhunjhunwala.uday@gmail.com>
2023-04-03 16:55:30 +0000

    basic arg parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use crate::TakeValue::*;
 use clap::{App, Arg};
 use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 type MyResult<T> = Result<T, Box< dyn Error>>;
 
@@ -32,14 +34,14 @@ pub fn get_args() -> MyResult<Config> {
                             .value_name("FILE")
                             .multiple(true)
                             .help("Input file(s)")
-                            .default_value("-")
+                            .required(true)
                     )
                     .arg(
 
                         Arg::with_name("lines")
                             .short("n")
                             .long("lines")
-                            .takes_value(true)
+                            .value_name("LINES")
                             .default_value("10")
                     )
                     .arg(
@@ -47,7 +49,8 @@ pub fn get_args() -> MyResult<Config> {
                         Arg::with_name("bytes")
                             .short("c")
                             .long("bytes")
-                            .takes_value(true)
+                            .value_name("BYTES")
+                            .conflicts_with("lines")
                     )
                     .arg(
                         Arg::with_name("quiet")
@@ -57,12 +60,19 @@ pub fn get_args() -> MyResult<Config> {
                     .get_matches();
     
     let files = matches.values_of_lossy("files").unwrap();
-    let lines = parse_num(&matches.value_of_lossy("lines").unwrap().into_owned())?;
+    let lines = parse_num_1(&matches.value_of("lines").unwrap())
+                                                .map_err(|e| {
+                                                    format!("illegal line count -- {}",e)
+                                                }
+    )?;
     let mut bytes = None;
 
     if matches.is_present("bytes") {
 
-        bytes = Some(parse_num(&matches.value_of_lossy("bytes").unwrap().into_owned())?);
+        bytes = Some(parse_num_1(&matches.value_of("bytes").unwrap())
+                        .map_err(|e| {
+                            format!("illegal byte count -- {}",e)
+                        })?);
     }
 
     let quiet = matches.is_present("quiet");
@@ -85,9 +95,19 @@ pub fn get_args() -> MyResult<Config> {
 
 pub fn run(config: Config) -> MyResult<()> {
 
-    println!("{:#?}", config);
+   for filename in config.files {
 
-    Ok(())
+    match File::open(&filename) {
+
+        Err(err) => eprintln!("{}: {}",filename,err),
+        Ok(_) =>{
+            
+            let (total_lines, total_bytes) = count_lines_and_bytes(&filename)?;
+            println!("{} has {} lines and {} bytes", filename, total_lines, total_bytes);
+        }
+    }
+   }
+   Ok(())
 }
 
 fn parse_num(num: &String) -> MyResult<TakeValue> {
@@ -105,3 +125,60 @@ fn parse_num(num: &String) -> MyResult<TakeValue> {
     Ok(x)
 
 }
+
+// Function that takes in a filename and returns a count of total lines and bytes in it
+
+fn count_lines_and_bytes(filename: &str) -> MyResult<(usize, usize)> {
+    
+        let mut file = Box::new(BufReader::new(File::open(filename)?));
+        let mut contents = String::new();
+        let mut num_lines = 0;
+        let mut num_bytes = 0;
+        loop {
+
+            let bytes = file.read_line(&mut contents)?;
+            if bytes == 0 {
+                break;
+            }
+            num_lines+=1;
+            num_bytes+= bytes;
+            contents.clear();
+        }
+        
+        let lines = num_lines;
+        let bytes = num_bytes;
+        Ok((lines, bytes))
+    
+}
+
+
+/*
+A function called parse_num_1 that takes in a &str value and returns a TakeValue
+The function should return a PlusZero if string is '+0' else it should return a TakeNum
+with the value of the string parsed as an i64 with negative wrapping
+Any error should be returned with string value of the number
+*/
+fn parse_num_1(num: &str) -> MyResult<TakeValue> {
+
+    let mut x;
+    if num == "+0" {
+
+        x = PlusZero;
+    }
+    else {
+
+        x = TakeNum(num.parse()?);
+    }
+
+    Ok(x)
+
+}
+
+/*
+A function called parse_num_2 that takes in a &str value and returns a TakeValue
+The function should return a PlusZero if string is '+0' else it should return a TakeNum
+with the value of the string (strings without '+' should be treated as negative numbers) parsed as an i64 
+Any error should be returned with string value of the number
+*/
+
+
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ fn main() {
     if let Err(e) = tailr::get_args().and_then(tailr::run) {
 
         eprintln!("{}",e);
-        std::process:exit(1);
+        std::process::exit(1);
     }
 }
